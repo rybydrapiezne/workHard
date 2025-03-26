a5ba17301890073ba797ff11496b94f22831a69c
rybydrapiezne <szymik43@gmail.com>
2025-03-26 15:30:43 +0100

    Background, first steps in AI nav

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -11,11 +11,15 @@ public class PlayerInteraction : MonoBehaviour
     Coroutine coroutine;
     GameObject colisionObject;
     float interactionSpeed = 1.0f;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         colisionObject = collision.gameObject;
-        interact.action.performed += timedInteraction;
-        interact.action.canceled += cancelTimedInteraction;
+        if (collision.tag == "Interactable")
+        {
+            interact.action.performed += timedInteraction;
+            interact.action.canceled += cancelTimedInteraction;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -25,17 +29,26 @@ public class PlayerInteraction : MonoBehaviour
 
     private void cancelTimedInteractionByExiting()
     {
-        StopCoroutine(coroutine);
-        colisionObject.GetComponent<SliderController>().stopSlider();
-        colisionObject.GetComponent<SliderEnabler>().disableSlider();
-        colisionObject = null;
+        if (colisionObject != null)
+        {
+            if (coroutine != null)
+                StopCoroutine(coroutine);
+            colisionObject.GetComponent<SliderController>().stopSlider();
+            colisionObject.GetComponent<SliderEnabler>().disableSlider();
+            colisionObject = null;
+            interact.action.performed -= timedInteraction;
+            interact.action.canceled -= cancelTimedInteraction;
+        }
     }
 
     private void cancelTimedInteraction(InputAction.CallbackContext context)
     {
-        colisionObject.GetComponent<SliderEnabler>().disableSlider();
-        StopCoroutine(coroutine);
-        colisionObject.GetComponent<SliderController>().stopSlider();
+        if (colisionObject != null)
+        {
+            colisionObject.GetComponent<SliderEnabler>().disableSlider();
+            StopCoroutine(coroutine);
+            colisionObject.GetComponent<SliderController>().stopSlider();
+        }
 
     }
 
@@ -59,6 +72,8 @@ public class PlayerInteraction : MonoBehaviour
             {
                 sliderController.finalizeInteraction();
                 sliderController.enabled = false;
+                interact.action.performed -= timedInteraction;
+                interact.action.canceled -= cancelTimedInteraction;
                 yield break;
             }
         }
